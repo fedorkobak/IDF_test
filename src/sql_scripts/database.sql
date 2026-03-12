f6f809843b27a393b73e35ae316c9942ac054838
fedorkobak <kobfedsur@gmail.com>
2026-03-12 23:05:21 +0300

    add materiazlized view

--- src/sql_scripts/database.sql
+++ src/sql_scripts/database.sql
@@ -4,3 +4,19 @@ CREATE TABLE IF NOT EXISTS raw_api_responses (
 )
 ENGINE = ReplacingMergeTree
 ORDER BY id;
+
+CREATE TABLE IF NOT EXISTS people (
+    craft String,
+    name String,
+    _inserted_at DateTime
+)
+ENGINE = MergeTree
+ORDER BY (craft, name, _inserted_at);
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS people_mv
+TO people AS
+SELECT
+    JSONExtractString(toJSONString(raw_json), 'craft') AS craft,
+    JSONExtractString(toJSONString(raw_json), 'name') AS name,
+    now() AS _inserted_at
+FROM raw_api_responses;
